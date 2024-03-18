12d00d517a562dbac0a3f8ede76cf63ae044a084
Brithanny Arguello <49217000+BrithannyAL@users.noreply.github.com>
2024-03-17 23:22:55 -0600

    update

---
+++ src/components/CoverNotAvailable.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../CoverNotAvailable.css';
+
+const CoverNotAvailable = () => {
+  return (
+    <div className="simulacion">
+      <p className="texto">Portada no disponible</p>
+    </div>
+  );
+};
+
+export default CoverNotAvailable;
